167716bdc9eab76a4cd50d391a47a2366e7f7181
satendra <satendrakr.7@gmail.com>
2021-04-14 00:38:16 +0530

    fix indentation for getOddNumbers

--- select-odd-numbers.js
+++ select-odd-numbers.js
@@ -2,12 +2,13 @@ const numbers=[1,2,3,4,5,6,7,8,9];
 
 function getOddNumbers(numbers) {
  const oddnumbers=[];
- numbers.forEach(number=>{
-     if(number%2!=0)
-     {
+
+ numbers.forEach(number => {
+     if(number%2 != 0) {
          oddnumbers.push(number);
      }
  });
+
  return oddnumbers;
 }
 
