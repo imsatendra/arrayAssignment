1b2c4858a960651b235db9df845710c86d57affd
satendra <satendrakr.7@gmail.com>
2021-04-14 00:24:32 +0530

    add solution for getting odd numbers in array

---
+++ select-odd-numbers.js
@@ -0,0 +1,14 @@
+const numbers=[1,2,3,4,5,6,7,8,9];
+
+function getOddNumbers(numbers) {
+ const oddnumbers=[];
+ numbers.forEach(number=>{
+     if(number%2!=0)
+     {
+         oddnumbers.push(number);
+     }
+ });
+ return oddnumbers;
+}
+
+getOddNumbers(numbers);
