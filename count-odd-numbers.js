79ee07c970d08ccf20fcfeee8c981d47f131c71e
satendra <satendrakr.7@gmail.com>
2021-04-15 01:42:09 +0530

    refactor countOddNumbers

---
+++ count-odd-numbers.js
@@ -0,0 +1,13 @@
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+function countOddNumbers(numbers) {
+    let count = 0;
+    numbers.forEach(number => {
+        if(number%2 != 0) {
+            count++;
+        }
+    })
+    return count;
+}
+
+console.log(countOddNumbers(numbers));
