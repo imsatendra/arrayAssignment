db4db168261c953ceddd22bdd5bbc2ab8164e4b8
satendra <satendrakr.7@gmail.com>
2021-04-14 08:12:36 +0530

    getting a reverse array of a given array

---
+++ get-reverse-numbers.js
@@ -0,0 +1,16 @@
+const numbers= [1,2,3,4,5,6,7,8,9];
+const length=numbers.length;
+function getReverseNumber(numbers){
+    const reversenumbers=[];
+    numbers.forEach(number=>{
+        if(length>=0)
+        {
+        reversenumbers[length-1]=number;
+        length--;
+        }
+        
+    });
+    return reversenumbers;
+
+}
+console.log(getReverseNumber(numbers));
