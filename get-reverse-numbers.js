dbe301e3a755281e4f4c8b12c8113abc036b5ed9
satendra <satendrakr.7@gmail.com>
2021-04-14 12:31:46 +0530

    chnges in program

--- get-reverse-numbers.js
+++ get-reverse-numbers.js
@@ -1,10 +1,10 @@
-const numbers= [1,2,3,4,5,6,7,8,9];
-const length=numbers.length;
+const numbers = [1,2,3,4,5,6,7,8,9];
+const length = numbers.length;
 function getReverseNumber(numbers){
     const reversenumbers=[];
     numbers.forEach(number  => {
         if(length>=0) {
-        reversenumbers[length-1]=number;
+        reversenumbers[length-1] = number;
         length--;
         }
         
