ed761a70547fa210ebbbe4f3b74f658860de3ef5
satendra <satendrakr.7@gmail.com>
2021-04-16 20:31:24 +0530

    add solution for reversing a array

---
+++ get-reverse-numbers -in-array.js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3,4,5,6,7,8];
+
+function getReverseOfArray(number) {
+    const reverseNumbers = [];
+    numbers.forEach(number => {
+        reverseNumbers.unshift(number);
+    })
+}
