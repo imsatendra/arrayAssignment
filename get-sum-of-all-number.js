ce6c8654a794f37692ff092277007eb7c6d307f6
satendra <satendrakr.7@gmail.com>
2021-04-16 20:22:22 +0530

    add soultion for getting sum of all numbers in a array

---
+++ get-sum-of-all-number.js
@@ -0,0 +1,11 @@
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+function getSumOfAllNumbersInArray(numbers) {
+    let sum=0;
+    numbers.forEach(number => {
+        sum=sum+number;
+    });
+    return sum;
+}
+
+console.log(getSumOfAllNumbersInArray(numbers));
