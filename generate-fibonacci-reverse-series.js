8a60d82ab491f5a8f533c725f1ee5fd5953cb460
satendra <satendrakr.7@gmail.com>
2021-04-16 22:04:05 +0530

    refactor and fix indentation for generate fibonacci reverse series

--- generate-fibonacci-reverse-series.js
+++ generate-fibonacci-reverse-series.js
@@ -1,16 +1,17 @@
-let firstNumber = 0;
-let secondNumber = 1;
-
-function generateFibonacciReverseSeries(firstNumber,secondNumber) {
-    const n = 10;
+function generateFibonacciReverseSeries(n) {
+    let firstNumber = 0;
+    let secondNumber = 1;
     const fibonacciSeries = [];
-    for(index = 1; index <= n; index++) {
-        let thirdNumber = firstNumber+secondNumber;
-        fibonacciSeries.unshift(thirdNumber);
+    fibonacciSeries.unshift(0);
+    fibonacciSeries.unshift(1);
+    for(index = 2; index <= n; index++) {
+         let thirdNumber = firstNumber+secondNumber;
+         fibonacciSeries.unshift(thirdNumber);
         firstNumber = secondNumber;
         secondNumber = thirdNumber;
+       
     }
     return fibonacciSeries;
 }
  
-console.log(generateFibonacciReverseSeries(firstNumber,secondNumber));
+console.log(generateFibonacciReverseSeries(10));
