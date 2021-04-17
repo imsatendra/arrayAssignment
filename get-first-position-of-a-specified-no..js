e30a9e4d0364f27fe037e710276a0963a26d8d79
satendra <satendrakr.7@gmail.com>
2021-04-17 16:37:18 +0530

    add solution of find index of first position of specified number

---
+++ get-first-position-of-a-specified-no..js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3,1,1,2,2,4,5,4];
+
+function getIndexOfFirstPositionOfSpecifiedNumber(number) {
+   return  numbers.indexOf(number);
+}
+
+console.log(getIndexOfFirstPositionOfSpecifiedNumber(4));
+ 
