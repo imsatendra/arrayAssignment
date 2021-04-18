42d775330e0e882d203fbb0aff5acef93e15f543
satendra <satendrakr.7@gmail.com>
2021-04-18 14:46:52 +0530

    rename function to getIndexOfFirstOccurence and refactor same file

--- get-first-position-of-a-specified-no..js
+++ get-first-position-of-a-specified-no..js
@@ -1,8 +1,7 @@
 const numbers = [1,2,3,1,1,2,2,4,5,4];
 
-function getIndexOfFirstPositionOfSpecifiedNumber(number) {
-   return  numbers.indexOf(number);
+function getIndexOfFirstOccurrence(numbers, numberToCheck) {
+   return numbers.indexOf(numberToCheck);
 }
 
-console.log(getIndexOfFirstPositionOfSpecifiedNumber(4));
- 
+console.log(getIndexOfFirstOccurrence(numbers, 4));
