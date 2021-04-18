474fc850cefe9763f386f77f04d94d661ce2391b
satendra <satendrakr.7@gmail.com>
2021-04-18 21:39:51 +0530

    fix indentation and average formula to get average of numbers

--- get-average-of-numbers.js
+++ get-average-of-numbers.js
@@ -1,15 +1,11 @@
 const numbers = [1,2,3,4,5,6,7,8,9];
 
 function getAverage(numbers) {
-    let sum=0;
+    let sum = 0;
     numbers.forEach(number => {
-        sum = sum + number;
-        sum =sum+number;
-     
+        sum = sum + number; 
     })
-    return sum/2;   
+    return sum/numbers.length;   
 }
 
 console.log(getAverage(numbers));
-
-
