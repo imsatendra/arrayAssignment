5d8055e44057fa10ce3f01b5fd2672246c05c193
satendra <satendrakr.7@gmail.com>
2021-04-18 22:24:16 +0530

    rename variable in file count total numbers below a threshold values

--- count-total-numbers-below-a-threshold.js
+++ count-total-numbers-below-a-threshold.js
@@ -1,14 +1,14 @@
 const numbers = [1,2,3,4,5,6,7,8,9];
-const countOfNumbersBelowThreshold = 4;
+const threshold = 4;
 
-function countNumbersBelowThreshold(numbers , threshold) {
-    let countBelowTheNumber=0;
+function countNumbersBelowThreshold(numbers,threshold) {
+    let countOfNumbersBelowThreshold = 0;
     numbers.forEach(number => {
         if(number < threshold) {
-            countBelowTheNumber++;
+            countOfNumbersBelowThreshold++;
         }
     })
-    return countBelowTheNumber;
+    return  countOfNumbersBelowThreshold;
 }
 
-console.log(countNumbersBelowThreshold(numbers,countOfNumbersBelowThreshold));
+console.log(countNumbersBelowThreshold(numbers,threshold));
