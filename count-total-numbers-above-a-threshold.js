6397265ab7a851e0c4e65bebe9f19911d42b359c
satendra <satendrakr.7@gmail.com>
2021-04-18 22:29:26 +0530

    rename function and variable name in file count total numbers above a threshold number

--- count-total-numbers-above-a-threshold.js
+++ count-total-numbers-above-a-threshold.js
@@ -1,13 +1,14 @@
 const numbers = [1,2,3,4,5,6,7,8,9];
-const threshold = 4
-function countBelowNumbers(numbers, threshold) {
-    let countAboveTheNumber=0;
+const threshold = 4;
+
+function countNumbersAboveThreshold(numbers,threshold) {
+    let countOfNumbersAboveThreshold = 0;
     numbers.forEach(number => {
         if(number > threshold) {
-            countAboveTheNumber++;
+            countOfNumbersAboveThreshold++;
         }
     })
-    return countAboveTheNumber;
+    return countOfNumbersAboveThreshold;
 }
 
-console.log(countBelowNumbers(numbers,threshold));
+console.log(countNumbersAboveThreshold(numbers,threshold));
