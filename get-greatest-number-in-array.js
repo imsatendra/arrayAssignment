5f9288bfe3ab602052127d3b4e32c5850b166741
satendra <satendrakr.7@gmail.com>
2021-04-18 21:50:13 +0530

    fix indetation in file get greatest numbers in an array

--- get-greatest-number-in-array.js
+++ get-greatest-number-in-array.js
@@ -3,9 +3,9 @@ const numbers = [101,2,30,4,15,6,777,8];
 function findGreatestNuber(numbers) {
     let greatestNumber = numbers[0];
     numbers.forEach(number => {
-         if(greatestNumber <= number) {
-             greatestNumber=number;
-            }   
+        if(greatestNumber <= number) {
+            greatestNumber = number;
+        }   
     });
     return greatestNumber;
 }
