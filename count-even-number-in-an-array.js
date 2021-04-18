44a9b1007953522ac491e495baf771a888c776d2
satendra <satendrakr.7@gmail.com>
2021-04-18 16:13:25 +0530

    rename function name of count even number in an array

--- count-even-number-in-an-array.js
+++ count-even-number-in-an-array.js
@@ -1,6 +1,6 @@
 const numbers = [1,2,3,4,5,6,7,8,9];
 
-function countEvennumbers(numbers) {
+function countEvenNumbers(numbers) {
     let count = 0;
     numbers.forEach(number => {
         if(number%2 == 0) {
@@ -10,4 +10,4 @@ function countEvennumbers(numbers) {
     return count;
 }
 
-console.log(countEvennumbers(numbers));
+console.log(countEvenNumbers(numbers));
