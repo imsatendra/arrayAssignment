a4ffd4d979a31e1d9aaf03cbbd32b234c4cbd48f
satendra <satendrakr.7@gmail.com>
2021-04-20 14:26:17 +0530

    fix spelling mistake of function name in file get greatest number in array

--- get-greatest-number-in-array.js
+++ get-greatest-number-in-array.js
@@ -1,6 +1,6 @@
 const numbers = [101,2,30,4,15,6,777,8];
 
-function findGreatestNuber(numbers) {
+function findGreatestNumber(numbers) {
     let greatestNumber = numbers[0];
     numbers.forEach(number => {
         if(greatestNumber <= number) {
@@ -10,4 +10,4 @@ function findGreatestNuber(numbers) {
     return greatestNumber;
 }
 
-console.log(findGreatestNuber(numbers));
+console.log(findGreatestNumber(numbers));
