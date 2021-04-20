bcf3e805eb7a28f31748cd25b2d90c422b6dcd26
satendra <satendrakr.7@gmail.com>
2021-04-20 14:30:54 +0530

    fix indentation in file get sum of all number in an array

--- get-sum-of-all-number.js
+++ get-sum-of-all-number.js
@@ -1,9 +1,9 @@
 const numbers = [1,2,3,4,5,6,7,8,9];
 
 function getSumOfAllNumbersInArray(numbers) {
-    let sum=0;
+    let sum = 0;
     numbers.forEach(number => {
-        sum=sum+number;
+        sum = sum + number;
     });
     return sum;
 }
