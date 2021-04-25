2363aa727d82a0e355e082fd9d3e834d8025fe63
satendra <satendrakr.7@gmail.com>
2021-04-25 20:16:37 +0530

    used every method to solve check subset of an array

--- check-subSet.js
+++ check-subSet.js
@@ -1,15 +1,9 @@
 const array1 = [1,2,3,4,5];
-const array2 =[2,3,4,5];
+const array2 = [2,3,4,5];
 
 function checkSubset(array1, array2) {
     let isSubset = true;
-    return array2.every(number => {
-        if(array1.includes(number)) {
-            return isSubset;
-        } else {
-            return false;
-        }  
-    })
-}
-
+    return array2.every(number => array1.includes(number))    
+} 
+  
 console.log(checkSubset(array1, array2));
