182d7524bb8266a6e4ae24e35bc0ad39245b50a5
satendra <satendrakr.7@gmail.com>
2021-04-28 19:56:33 +0530

    add tests for get-first-position-of-specified-number

---
+++ test/get-first-position-of-specified-number.test.js
@@ -0,0 +1,6 @@
+const assert = require("assert");
+
+const { getIndexOfFirstOccurrence } = require('../src/get-first-position-of-a-specified-no.');
+
+assert.strictEqual(getIndexOfFirstOccurrence([1,2,3,4,5,6,7,4,7,9], 4), 3);
+assert.strictEqual(getIndexOfFirstOccurrence([1,2,3,4,5,6,7,4,7,9], 7), 6);
