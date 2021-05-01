22fa858cce7895a6ccdb7be28e4921697399f517
satendra <satendrakr.7@gmail.com>
2021-05-01 22:53:39 +0530

    add test for a case when threshold value is greater than all values of given array in count-total number above a threshold

--- test/count-total-numbers-above-a-threshold.test.js
+++ test/count-total-numbers-above-a-threshold.test.js
@@ -4,3 +4,4 @@ const { countNumbersAboveThreshold } = require('../src/count-total-numbers-above
 
 assert.strictEqual(countNumbersAboveThreshold([1,2,3,4,5,6,7,8,9], 4), 5);
 assert.strictEqual(countNumbersAboveThreshold([1,2,3,4,5,6,7,8,9], 1), 8);
+assert.strictEqual(countNumbersAboveThreshold([1,2,3,4,5,6,7,8,9], 10), 0);
