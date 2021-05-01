13fc3e6c874b39129f6986823606e91825364d5e
satendra <satendrakr.7@gmail.com>
2021-05-01 22:48:49 +0530

    add test cases for negative values as well in get-sum-of-number

--- test/get-sum-of-all-number.test.js
+++ test/get-sum-of-all-number.test.js
@@ -3,4 +3,5 @@ const assert = require("assert");
 const { getSumOfAllNumbersInArray } = require('../src/get-sum-of-all-number');
 
 assert.strictEqual(getSumOfAllNumbersInArray([1,2,3,4,5]), 15);
-assert.strictEqual(getSumOfAllNumbersInArray([0,1,2,3,4,5]),15);
+assert.strictEqual(getSumOfAllNumbersInArray([0,1,2,3,4,5]), 15);
+assert.strictEqual(getSumOfAllNumbersInArray([0,1,-0,3,-9,5]), 0);
