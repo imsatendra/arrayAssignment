4988de434c09eda3bc1f17a7f8aa006faee96bd2
satendra <satendrakr.7@gmail.com>
2021-05-01 22:58:13 +0530

    add test case for a value that doesn't exist in given array of file get- first-position-of-specified-number

--- test/get-first-position-of-specified-number.test.js
+++ test/get-first-position-of-specified-number.test.js
@@ -4,3 +4,4 @@ const { getIndexOfFirstOccurrence } = require('../src/get-first-position-of-a-sp
 
 assert.strictEqual(getIndexOfFirstOccurrence([1,2,3,4,5,6,7,4,7,9], 4), 3);
 assert.strictEqual(getIndexOfFirstOccurrence([1,2,3,4,5,6,7,4,7,9], 7), 6);
+assert.strictEqual(getIndexOfFirstOccurrence([1,2,3,4,5,6,7,4,7,9], 10), -1);
